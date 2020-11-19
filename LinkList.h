892490ac0d2bbe8e99a203e067631b60c9755da7
MY-stone <2674552955@qq.com>
2020-11-19 17:07:06 +0800

    master

--- LinkList.h
+++ LinkList.h
@@ -1,18 +1,41 @@
-#ifndef __LinkList.h__
-#define __LinkList.h__
+#ifndef __LINKLIST_H__
+#define __LINKLIST_H__
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-// //定义链表的必要元素
-// typedef struct LinkList_Student
-// {
-//     const char student_number;
-//     const string name;
-//     const string gender;
-//     const int age;
-//     const string subjet;
-//     int grade;
-//     struct LinkList *next;
-// };
+class Student
+{
+    private:
 
+        //定义链表的必要元素
+        typedef struct LinkList_Student
+        {
+            const char student_number;
+            const string name;
+            const string gender;
+            const int age;
+            const string subjet;
+            int grade;
+            struct LinkList_Student *next;
+        }LinkList_Student,*List;
 
+    private:
+        int size = 100;
+        //LinkList_Student Student_List;
+
+        /* data */
+    public:
+
+        void setSize(int setSize);
+        int getSize(void);
+        void Init_List(List &L);
+        void OutPut(void);
+        Student();
+        ~Student();
+
+};
 
 #endif
+
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include "LinkList.h"
+
+using namespace std;
+
+int main()
+{
+    Student s ;
+    s.OutPut();
+    getchar();
+    getchar();
+
+}
+
+
