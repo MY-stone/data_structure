021b286e28071a4d7abc56c997ef7e59ba1e232a
MY-stone <2674552955@qq.com>
2020-11-20 22:03:19 +0800

    master

--- LinkList.cpp
+++ LinkList.cpp
@@ -46,10 +46,13 @@ void Student::Init_List(void)
 
 void Student::Input_List(void)
 {
-	List p, r;
+	List p, L;
+    L = &(this->test_Score); 
 	fstream file;
     string s_data;
+    stringstream s_change;
     vector<string> vs_data;      //初始化容器
+
 	file.open(this->file_addreess);
 	if (!file) {
 		cout << "未找到相关文件，无法打开！检查文件路径！" << endl;
@@ -59,8 +62,28 @@ void Student::Input_List(void)
     getline(file,s_data);   //跳过第一行
     while (file.good()) {
         getline(file,s_data);
-        this->split(s_data.data(),vs_data);
+        this->split(s_data.data(),vs_data); //字符串
+
+        p = new LinkList_Student;
+        s_change<<vs_data[0];  //将string转换成其它类型
+        s_change>>p->student_number; //学号
+
+        p->name = vs_data[1];       //名字
+        p->gender = vs_data[2];     //性别
+
+        s_change<<vs_data[3];       //年龄
+        s_change>>p->age;
+
+        p->subjet = vs_data[4];     //科目
         
+        s_change<<vs_data[5];       //分数
+        s_change>>p->grade;
+        p->next = NULL;
+        vs_data.clear();            //清空容器
+
+        L->next = p;
+        L = p;
+
 	}
 
 	file.close();
